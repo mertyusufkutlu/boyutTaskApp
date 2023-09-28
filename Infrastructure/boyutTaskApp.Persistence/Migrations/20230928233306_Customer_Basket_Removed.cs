29a1aca2843212fc3c9097b42fe051975953fbb2
Mert <mertyusufkutlu@gmail.com>
2023-09-29 02:38:25 +0300

    fix(Customer_Basket): mig removed

---
+++ Infrastructure/boyutTaskApp.Persistence/Migrations/20230928233306_Customer_Basket_Removed.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace boyutTaskAppAPI.Persistence.Migrations
+{
+    public partial class Customer_Basket_Removed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
